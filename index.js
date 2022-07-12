b381b7e4eb66aaa9384faf09e469e0984fc979ff
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-07-12 11:30:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/* alert */
+alert("Good Morning People");
+
+/* number */
+2+3;
+
+/*declaring variables and storing data  */
+var myname = "Khushi"
+var yourname = prompt("What is your name?");
+
+/*Contatenation propert of string */
+alert("My name is " + myname + " welcome to my course " + yourname);
+
+
