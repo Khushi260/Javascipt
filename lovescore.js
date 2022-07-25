9656ed09881f40ab8e13959abc997230d52e9816
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-07-25 17:21:56 +0530

    using conditional statements

--- lovescore.js
+++ lovescore.js
@@ -8,3 +8,16 @@ var lovescore = Math.random()*100;
 lovescore = Math.floor(lovescore)+1;
 
 alert("Your lovescore is " + lovescore+"% .");
+
+//using conditional statements
+
+if (lovescore>=70){
+    alert("Your lovescore is " + lovescore + "% .  You both love each other like Kanye loves Kanye.");
+}
+else if(lovescore>49){
+      alert("Your lovescore is " + lovescore + "% . You love them more.");
+}
+
+else{
+    alert("Your lovescore is " + lovescore+"% .");
+}
