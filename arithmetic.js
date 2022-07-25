10884868257646d07ccc27eb6f1480a8ee0c44f8
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-07-25 17:09:38 +0530

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,6 @@
+
+var dogage = prompt("What is age of your dog?");
+var humanage = ((dogage-2)*4)+21;
+alert ("Your dog is " + humanage + "years old in human years.");
+
+//Increment Decrement Expressions
---
+++ lovescore.js
@@ -0,0 +1,10 @@
+//using random number generation
+
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+var lovescore = Math.random()*100;
+lovescore = Math.floor(lovescore)+1;
+
+alert("Your lovescore is " + lovescore+"% .");
