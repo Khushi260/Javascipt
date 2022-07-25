2f81d24683fb656c9c8fde00d5af3bb8075c303a
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-07-25 17:55:28 +0530

    Create leap year.js

---
+++ leap year.js
@@ -0,0 +1,23 @@
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+   if(year%4==0){
+       if(year%100==0){
+           if (year%400==0){
+               return "Leap year."
+           }
+           else{
+               return "Not leap year."
+           }
+       }
+       else{
+           return "Leap year."
+       }
+   }
+   else{
+       return "Not leap year."
+   }
+
+/**************Don't change the code below****************/    
+
+}
