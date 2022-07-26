ee083a3815bf1aac69722a6e96c5480ccd6247fd
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-07-26 15:14:56 +0530

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -5,6 +5,10 @@ function Bellboy (name, age, city, language, haspermit){
     this.city=city;
     this.haspermit=haspermit;
     this.language=language;
+    //method
+    this.cleaning = function(){
+        alert("Cleaning is in the progress....")
+    }
 }
 
 var bellboy1 = new Bellboy("Timmy",19,"Texas",["english","german","french"],"yes");
