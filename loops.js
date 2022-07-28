a60eccce30d0b85f901206973a1d5d7a0f42d145
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-07-28 23:02:36 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -3,3 +3,15 @@
 for(let i=0; i<5; i++){
     console.log("Hello");
 }
+
+//program to add first n natural numbers
+
+let sum =0;
+let n = prompt("Enter value of n");
+n = Number.parseInt(n)
+
+for( let i=1; i<n; i++){
+    sum = sum + i;
+}
+
+console.log("Sum of first" + n + " natural numbers is " + sum);
